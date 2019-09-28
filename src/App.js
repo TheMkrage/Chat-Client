daf3c32ab7ff8fa961343ce5adefa10c62ae9f31
Matthew Krager <matthewkrager@gmail.com>
2019-09-28 16:20:45 -0700

    working chat app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Websocket from 'react-websocket';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      messages: []
+    }
+  }
+
+  // Form Code
+  handleChange(event) {
+    this.setState({message: event.target.value});
+  }
+
+  handleSubmit(event) {
+    this.refWebSocket.sendMessage(this.state.message);
+    this.setState({message: ''})
+    event.preventDefault();
+  }
+
+  // Handle Websocket data
+  handleData(data) {
+    console.log(data);
+
+    this.setState(state => {
+      const messages = [...state.messages, data];
+      console.log(messages)
+      return {
+        messages
+      }
+    })
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Websocket url='ws://127.0.0.1:3012'
+              onMessage={this.handleData.bind(this)}
+              ref={Websocket => {
+                this.refWebSocket = Websocket;
+              }}
+          />
+
+
+          <ul style={{listStyleType: "none"}}>
+            {
+              this.state.messages.map((msg) =>
+                <li key={msg}>{msg}</li>
+              )
+            }
+          </ul>
+
+          <form onSubmit={this.handleSubmit.bind(this)}>
+            <label>
+              Message:
+              <input type="text" value={this.state.message} onChange={this.handleChange.bind(this)}/>
+            </label>
+            <input type="submit" value="Send!" />
+          </form>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
